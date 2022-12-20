61505688bee605a844cec8caae9686982d188e80
Jimin Kim <jmkim000000@gmail.com>
2022-12-20 17:03:44 +0900

    server 폴더 추가

---
+++ server/seb41-41-pre-project/src/main/java/seb4141preproject/Seb4141PreProjectApplication.java
@@ -0,0 +1,13 @@
+package seb4141preproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb4141PreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb4141PreProjectApplication.class, args);
+	}
+
+}
---
+++ server/seb41-41-pre-project/src/test/java/seb4141preproject/Seb4141PreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package seb4141preproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb4141PreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
