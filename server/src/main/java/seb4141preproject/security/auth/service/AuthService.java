3a7098c9198596bce2106a197a276ec20f63d2a8
DreamChaserDeekay <110885981+DreamChaserDeekay@users.noreply.github.com>
2022-12-26 16:50:24 +0900

    Feat/authorize requests (#27)
    * refactor : api 명세서 수정에 따른 SecurityConfig 권한 부여 url 수정
    comment : AuthService 주석 수정
    
    * feat : refresh token 쿠키 생성 로직 초안 작성

--- server/src/main/java/seb4141preproject/security/auth/service/AuthService.java
+++ server/src/main/java/seb4141preproject/security/auth/service/AuthService.java
@@ -13,6 +13,8 @@ import seb4141preproject.security.auth.dto.*;
 import seb4141preproject.security.auth.entity.*;
 import seb4141preproject.security.auth.repository.RefreshTokenRepository;
 
+import javax.servlet.http.Cookie;
+
 @Service
 @RequiredArgsConstructor
 public class AuthService {
@@ -24,11 +26,10 @@ public class AuthService {
     private final MemberMapper mapper;
 
     public TokenDto login(LoginDto loginDto) {
-        // 1. Login ID/PW 를 기반으로 AuthenticationToken 생성
+        // 1. loginDto 기반 authenticationToken 생성 (toAuthentication 메소드 활용)
         UsernamePasswordAuthenticationToken authenticationToken = loginDto.toAuthentication();
 
-        // 2. 실제로 검증 (사용자 비밀번호 체크) 이 이루어지는 부분
-        //    authenticate 메서드가 실행이 될 때 CustomUserDetailsService 에서 만들었던 loadUserByUsername 메서드가 실행됨
+        // 2. token 검증
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
         // 3. 인증 정보를 기반으로 JWT 토큰 생성
@@ -85,6 +86,14 @@ public class AuthService {
                 .orElseThrow(() -> new RuntimeException("REFRESH_TOKEN_NOT_FOUND"));
 
         refreshTokenRepository.delete(refreshToken);
+    }
+
+    // refresh token Cookie 생성 로직
+    public Cookie createCookie(TokenDto tokenDto) {
+        Cookie cookie = new Cookie("refresh-token", tokenDto.getRefreshToken());
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        return cookie;
     }
 
     // 클래스 내부에서만 사용 가능한 토큰 생성하는 로직
--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -1,7 +1,7 @@
-insert into member (member_id, name, email, password)
-values
-(1, '홍길동1', 'hgd1@asdf.com', '1111'),
-(2, '홍길동2', 'hgd2@asdf.com', '2222'),
-(3, '홍길동3', 'hgd3@asdf.com', '3333'),
-(4, '홍길동4', 'hgd4@asdf.com', '4444'),
-(5, '홍길동5', 'hgd5@asdf.com', '5555');
+--insert into member (member_id, name, email, password)
+--values
+--(1, '홍길동1', 'hgd1@asdf.com', '1111'),
+--(2, '홍길동2', 'hgd2@asdf.com', '2222'),
+--(3, '홍길동3', 'hgd3@asdf.com', '3333'),
+--(4, '홍길동4', 'hgd4@asdf.com', '4444'),
+--(5, '홍길동5', 'hgd5@asdf.com', '5555');
