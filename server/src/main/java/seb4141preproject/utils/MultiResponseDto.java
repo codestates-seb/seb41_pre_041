8d556d42d9ba453fcb2a3f61ea985b9f79dd9506
Jimin Kim <88694161+jmkim0@users.noreply.github.com>
2022-12-27 13:52:47 +0900

    refactor: utils 정리 (#32)
    MultiResponseDto, AuditingConfig utils로 이동
    페이지네이션된 응답은 MultiResponseDto로 통일
    PageInfo를 static nested class로 변경
    SingleResponseDto 제거

---
+++ server/src/main/java/seb4141preproject/utils/MultiResponseDto.java
@@ -0,0 +1,30 @@
+package seb4141preproject.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+public class MultiResponseDto<T> {
+
+    private final List<T> data;
+
+    private final PageInfo pageInfo;
+
+    public MultiResponseDto(List<T> data, Page<?> page) {
+        this.data = data;
+        this.pageInfo = new PageInfo(page.getNumber() + 1,
+                page.getSize(), page.getTotalElements(), page.getTotalPages());
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class PageInfo {
+        private int page;
+        private int size;
+        private long totalElements;
+        private int totalPages;
+    }
+}
