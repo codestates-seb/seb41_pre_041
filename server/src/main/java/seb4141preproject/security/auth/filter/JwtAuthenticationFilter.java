397a294ad92d853bcbb7b16088b200837e2a97bb
DreamChaserDeekay <deekayshin@gmail.com>
2022-12-31 16:23:35 +0900

    refactor : auths 단 refactoring 1. JwtExceptionHandlerFilter.java 추가 - JwtAuthenticationFilter.java 와 역할 분리
    2. AuthService.java 단 예외 처리 구체화
    - JwtExceptionAdvice.java 에서 처리
    
    3. JwtAuthenticationFilter.java - shouldNotFilter 조건 추가
    - 로컬에서 h2 console 을 이용하여 테스트 시, accessToken null 예외가 발생해서 필터링 되지 않도록 처리
    
    4. 로그 추가

--- server/src/main/java/seb4141preproject/security/auth/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/seb4141preproject/security/auth/filter/JwtAuthenticationFilter.java
@@ -1,14 +1,12 @@
 package seb4141preproject.security.auth.filter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import seb4141preproject.security.auth.dto.TokenDto;
 import seb4141preproject.security.auth.provider.*;
-import seb4141preproject.security.auth.service.AuthService;
-import seb4141preproject.security.auth.utils.HeaderMapRequestWrapper;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -18,9 +16,9 @@ import java.io.IOException;
 import java.util.Arrays;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenizer jwtTokenizer;
-    private final AuthService authService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -28,30 +26,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
 
         String accessToken = resolveAccessToken(request); // request header 에서 accessToken 추출
-        HeaderMapRequestWrapper wrapper = new HeaderMapRequestWrapper(request); // (reissue 시) header 값 덮어쓰기를 위한 wrapper 객체 생성
 
-        int validateResult = jwtTokenizer.validateToken(accessToken);
+        log.info("Authentication Filter validateToken 실행");
 
-        if (validateResult == 0) { // validateToken 이상 없을 경우
-
+        if (jwtTokenizer.validateToken(accessToken)) {
             Authentication authentication = jwtTokenizer.getAuthentication(accessToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        } else if (validateResult == 1) { // jwt 토큰이 만료되었을 경우
-
-            String refreshToken = resolveRefreshToken(request); // request header 에서 refreshToken 추출
-
-            Authentication authentication = jwtTokenizer.getAuthentication(refreshToken); // refreshToken 으로 authentication 생성
-            // -> 만료된 accessToken 으로는 getAuthentication 불가능하기 때문
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            TokenDto tokenDto = authService.reissue(request, authentication); // 토큰 재발급
-
-            setHeaderResponse(wrapper, response, tokenDto); // header, response 값 재설정
-
         }
-        // validateToken 결과값이 2(이상 없음 or 토큰 만료가 아닌 겅우)인 경우 바로 다음 필터링 진행
-        filterChain.doFilter(wrapper, response);
+        filterChain.doFilter(request, response);
     }
 
     @Override
@@ -59,9 +41,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String path = request.getRequestURI(); // URL
         String method = request.getMethod(); // request 의 메소드 종류
-        String[] urls = new String[] {"/api/members", "/api/auths/login"}; // 회원가입, 로그인은 토큰이 필요 없음
+        String[] urls = new String[] {"/api/members", "/api/auths/login", "/h2.*"}; // 회원가입, 로그인, h2는 토큰이 필요 없음
 
-        return Arrays.stream(urls).anyMatch(s -> s.equals(path)) || method.equals("GET");
+        log.info("path : " + path);
+        log.info("match ? : " + Arrays.stream(urls).anyMatch(s -> path.matches(s)));
+
+        return Arrays.stream(urls).anyMatch(s -> path.matches(s))
+                || (!path.matches(".*/votes/me") && method.equals("GET"));
     }
 
     private String resolveAccessToken(HttpServletRequest request) {
@@ -71,27 +57,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         return null;
-    }
-
-    private String resolveRefreshToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("RefreshToken");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-
-        return null;
-    }
-
-    private void setHeaderResponse(HeaderMapRequestWrapper wrapper,
-                                   HttpServletResponse response,
-                                   TokenDto tokenDto) {
-
-        // accessToken, refreshToken Header 값 새로 덮어쓰기
-        wrapper.addHeader("Authorization", tokenDto.getAccessToken());
-        wrapper.addHeader("RefreshToken", tokenDto.getRefreshToken());
-
-        // reissue 후 response header 에 새로운 accessToken, refreshToken 값 추가
-        response.addHeader("Authorization", tokenDto.getAccessToken());
-        response.addHeader("RefreshToken", tokenDto.getRefreshToken());
     }
 }
--- server/src/main/java/seb4141preproject/security/auth/handler/AuthFailureHandler.java
+++ server/src/main/java/seb4141preproject/security/auth/handler/AuthFailureHandler.java
@@ -2,12 +2,10 @@ package seb4141preproject.security.auth.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import seb4141preproject.security.auth.utils.*;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
