863c6f928267d9cb5df166d1ea52ac36a33e971f
DreamChaserDeekay <deekayshin@gmail.com>
2022-12-31 21:18:11 +0900

    fix : Cors Configuration 추가

--- server/src/main/java/seb4141preproject/security/auth/config/SecurityConfig.java
+++ server/src/main/java/seb4141preproject/security/auth/config/SecurityConfig.java
@@ -3,16 +3,22 @@ package seb4141preproject.security.auth.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import seb4141preproject.security.auth.handler.CustomAccessDeniedHandler;
 import seb4141preproject.security.auth.handler.CustomAuthenticationEntryPoint;
 import seb4141preproject.security.auth.provider.JwtTokenizer;
 import seb4141preproject.security.auth.service.AuthService;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -25,6 +31,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .headers().frameOptions().disable()
+                .and()
+
+                .cors().configurationSource(corsConfigurationSource())
                 .and()
 
                 .csrf().disable()
@@ -68,6 +77,20 @@ public class SecurityConfig {
                 .disable();
 
         return http.build();
+    }
+
+    CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "refreshToken"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Bean
